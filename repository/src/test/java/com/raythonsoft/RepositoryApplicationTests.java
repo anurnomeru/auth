b2665a12f54e5d3341b0e802296938054743b922
Anur <anurnomeru@163.com>
2018-01-18 21:28:11 +0800

    认证filter还有点问题，httpclient不知道为什么用不了

---
+++ repository/src/test/java/com/raythonsoft/RepositoryApplicationTests.java
@@ -0,0 +1,17 @@
+package com.raythonsoft;
+
+import com.raythonsoft.repository.RepositoryApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = RepositoryApplication.class)
+public class RepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
