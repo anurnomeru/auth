38b62ff40eb584507ae41031601f3b088fffab8c
Anur <anurnomeru@163.com>
2018-01-26 22:21:55 +0800

    初始化权限的切面

--- sso/src/main/java/com/raythonsoft/sso/interceptor/LogAspect.java
+++ sso/src/main/java/com/raythonsoft/sso/interceptor/LogAspect.java
@@ -4,16 +4,20 @@ import com.alibaba.fastjson.JSON;
 import com.raythonsoft.repository.model.Log;
 import com.raythonsoft.repository.service.LogService;
 import com.raythonsoft.repository.util.NetworkUtil;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.log4j.Log4j;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Date;
 
 /**
@@ -42,6 +46,10 @@ public class LogAspect {
                 .userAgent(request.getHeader("User-Agent"))
                 .username(JSON.toJSONString(request.getUserPrincipal())).build();
 
-
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        if (method.isAnnotationPresent(ApiOperation.class)) {
+            log.setDescription(method.getAnnotation(ApiOperation.class).value());
+        }
     }
 }
